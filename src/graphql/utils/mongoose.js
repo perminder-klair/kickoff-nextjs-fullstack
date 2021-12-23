93d553b3e677db0312319a1b632d020c54245e93
Parminder Klair <perminder.klair@gmail.com>
2021-12-23 17:45:20 +0000

    Update mongoose.js

--- src/graphql/utils/mongoose.js
+++ src/graphql/utils/mongoose.js
@@ -1,15 +1,27 @@
 import mongoose from 'mongoose';
 
-function connectDatabase(uri) {
-  mongoose.connection
-    .on('error', (error) => console.log(error))
-    .on('close', () => console.log('Database connection closed.'))
-    .once('open', () => {
-      const info = mongoose.connections[0];
-      console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
-    });
+let cached = global.mongoose;
 
-  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+if (!cached) {
+  // eslint-disable-next-line no-multi-assign
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDatabase(uri) {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    cached.promise = mongoose.connect(uri, opts).then((mongo) => mongo);
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
 export default connectDatabase;
