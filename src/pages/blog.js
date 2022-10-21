0dc491425dfb8c8cf3319c5a413f5768ac71b85a
Parminder Klair <perminder.klair@gmail.com>
2022-10-21 13:46:25 +0100

    buld fix

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -19,29 +19,27 @@ const query = gql`
   }
 `;
 
-const Blog = ({ posts }) => {
-  return (
-    <Layout>
-      <NextSeo
-        title="Blog"
-        description={`Get in touch with us at ${config.siteName}`}
-        url={`${config.siteUrl}/blog`}
-      />
-      <section tw="text-gray-600">
-        <div tw="container px-5 py-24 mx-auto">
-          <div tw="flex flex-col text-center w-full mb-12">
-            <Heading type="h1">News & Updates</Heading>
-          </div>
-          <div tw="flex flex-wrap -m-4">
-            {posts.map((post) => (
-              <NewsItem key={post.id} post={post} />
-            ))}
-          </div>
+const Blog = ({ posts }) => (
+  <Layout>
+    <NextSeo
+      title="Blog"
+      description={`Get in touch with us at ${config.siteName}`}
+      url={`${config.siteUrl}/blog`}
+    />
+    <section tw="text-gray-600">
+      <div tw="container px-5 py-24 mx-auto">
+        <div tw="flex flex-col text-center w-full mb-12">
+          <Heading type="h1">News & Updates</Heading>
         </div>
-      </section>
-    </Layout>
-  );
-};
+        <div tw="flex flex-wrap -m-4">
+          {posts.map((post) => (
+            <NewsItem key={post.id} post={post} />
+          ))}
+        </div>
+      </div>
+    </section>
+  </Layout>
+);
 
 export default Blog;
 
